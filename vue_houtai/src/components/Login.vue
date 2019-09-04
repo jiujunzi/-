85ec4563507926a9c6dcfb1de52fcf6ca53c6f5d
jiujunzi <1799972682@qq.com>
2019-09-04 23:12:36 +0800

    完成登录操作，数据提交

--- vue_houtai/src/components/Login.vue
+++ vue_houtai/src/components/Login.vue
@@ -7,19 +7,23 @@
       </div>
 
       <!-- 表单区域 -->
-      <el-form class="login_form">
+      <el-form ref="loginFormRef" class="login_form" :rules="loginFormRules" :model="loginForm">
         <!-- 用户名 -->
-        <el-form-item>
-          <el-input prefix-icon="iconfont icon-user"></el-input>
+        <el-form-item prop="username">
+          <el-input prefix-icon="iconfont icon-user" v-model="loginForm.username"></el-input>
         </el-form-item>
         <!-- 密码 -->
-         <el-form-item>
-          <el-input prefix-icon="iconfont icon-3702mima"></el-input>
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="iconfont icon-3702mima"
+          ></el-input>
         </el-form-item>
         <!-- 按钮 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -28,9 +32,48 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      //登录表单数据对象
+      loginForm: {
+        username: "admin",
+        password: "123456"
+      },
+      // 表单验证规则
+      loginFormRules: {
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" },
+          { min: 3, max: 10, message: "长度在 3 到 10 个字符", trigger: "blur" }
+        ],
+        password: [
+          { required: true, message: "请输入密码", trigger: "blur" },
+          { min: 6, max: 10, message: "长度在 6 到 10 个字符", trigger: "blur" }
+        ]
+      }
+    };
   },
-  methods: {}
+  methods: {
+    //重置表单功能
+    resetLoginForm() {
+      //console.log(this.$refs.resetLoginForm)
+      this.$refs.loginFormRef.resetFields();
+    },
+    //登录按钮预验证
+    // valid是布尔值类型的行参，箭头函数只有一个参数时，可以省略括号
+    login() {
+      this.$refs.loginFormRef.validate(async valid => {
+        //console.log(valid)
+        if (!valid) return;
+        // 解构data给res，async和await，返回的是promise对象
+        const { data: res } = await this.$http.post("login", this.loginForm);
+        console.log(res);
+        // 显示登录结果
+        if (res.meta.status !==200 ) return this.$message.error(res.meta.msg)
+        this.$message.success(res.meta.msg)
+
+        
+      });
+    }
+  }
 };
 </script>
 <style lang='less' scoped>
