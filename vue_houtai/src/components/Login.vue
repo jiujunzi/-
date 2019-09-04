e4c4d280d7f16ba826fae07df15fcc1e95ae3945
jiujunzi <1799972682@qq.com>
2019-09-04 14:36:15 +0800

    登录组件路由

---
+++ vue_houtai/src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    denglu
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+  
+    };
+  },
+  methods:{
+  
+  }
+}
+</script>
+<style lang='less' scoped>
+  
+</style>
